ac5150701e62370ca966795d49622a92b6277f67
catherinecunningham <cfunk512@gmail.com>
2022-09-21 19:33:27 -0700

    update all pages for today

--- src/page-template.js
+++ src/page-template.js
@@ -4,7 +4,7 @@ const generateTeam = team => {
     <div class="card employee-card">
     <div class="card-header">
         <h2 class="card-title">${manager.getName()}</h2>
-        <h3 class="card-title><i class="`
+        <h3 class="card-title><i class="` //finish employee card set up
     }
 
     const generateEngineer = engineer => {
