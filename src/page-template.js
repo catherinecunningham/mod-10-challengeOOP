d2d915f2273be90d58ac15e7531a6b377bc88439
catherinecunningham <cfunk512@gmail.com>
2022-09-21 19:13:12 -0700

    start page template file

---
+++ src/page-template.js
@@ -0,0 +1,17 @@
+const generateTeam = team => {
+    const generateManager = manager => {
+        return `
+    <div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${manager.getName()}</h2>
+        <h3 class="card-title><i class="`
+    }
+
+    const generateEngineer = engineer => {
+
+    }
+
+    const generateIntern = intern => {
+
+    }
+}
